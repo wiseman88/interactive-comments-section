97a58d7f8cf89220477c3441cbabc07fafc6ac96
wiseman88 <wiseadam02@gmail.com>
2022-11-11 16:09:09 +0100

    fix: fix sass load

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <CommentList />
+  <div class="comments-wrapper">
+    <CommentList />
+  </div>
 </template>
 
 <script setup>
@@ -7,12 +9,13 @@ import CommentList from './components/comment/CommentList.vue';
 </script>
 
 <style lang="scss">
-@import './assets/styles/reset.css';
-@import './assets/styles/app.scss';
-
 .title {
   font-size: 4rem;
   font-weight: 400;
   font-family: 'Rubik', serif;
+}
+
+.comments-wrapper {
+  padding: 31px 15px;
 }
 </style>
--- src/components/comment/Comment.vue
+++ src/components/comment/Comment.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-
+    <div class="comment-content">
+        <div class="comment-header">
+            <figure class="mr-1">
+                <img class="avatar" src="/public/assets/images/avatars/image-amyrobson.png" alt="">
+            </figure>
+            <span class="mr-1 lowercase">
+                Amy robson
+            </span>
+            <span class="lowercase text-primary-moderate">
+                1 month ago
+            </span>
+        </div>
+        <p class="comment-body text-primary-moderate">
+            Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've
+            nailed the design and the responsiveness at various breakpoints works really well.
+        </p>
     </div>
 </template>
 
