ef7792ac65a3c06a0727ab7734facfab326546ae
wiseman88 <wiseadam02@gmail.com>
2022-11-12 18:49:22 +0100

    feat: add comment replies

--- src/components/comment/CommentList.vue
+++ src/components/comment/CommentList.vue
@@ -1,6 +1,11 @@
 <template>
     <div v-for="comment in data.comments" :key="comment.id">
         <Comment :comment="comment" />
+        <div class="replies-container" v-if="comment.replies.length">
+            <div v-for="reply in comment.replies" :key="reply.id">
+                <Comment :comment="reply" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,5 +17,8 @@ const data = useCommentStore();
 </script>
 
 <style lang="scss" scoped>
-
+.replies-container {
+    padding-left: 1rem;
+    border-left: 1px solid #E8E9ED;
+}
 </style>
